ef6a83609b46362bb255efd58df62d00f90e373f
Martamp <motymont@gmail.com>
2020-04-10 21:34:19 +0200

    introducing background and starting cursor movement

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,5 @@
-'use strict';
-
-console.log('>> Ready :)');
+"use strict";
+const cursor = document.querySelector(".cursor");
+cursor.addEventListener("mousemove", (ev) => {
+  cursor.setAttribute("style", `top: ${ev.page} px; left: ${ev.pageX}`);
+});
